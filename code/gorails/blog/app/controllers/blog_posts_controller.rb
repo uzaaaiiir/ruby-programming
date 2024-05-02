671ee5519e24846240f24666381fc27217081a13
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2024-05-02 11:27:00 -0400

    Scheduled psots in blog, destroy logic, tailwindcss gem

--- code/gorails/blog/app/controllers/blog_posts_controller.rb
+++ code/gorails/blog/app/controllers/blog_posts_controller.rb
@@ -1,12 +1,12 @@
 class BlogPostsController < ApplicationController
-    def index 
-        @blog_posts = BlogPost.all
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_blog_post, except: [:index, :new, :create] # only: [:show, :edit, :update, :destroy] 
+
+    def index
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
 
     def show 
-        @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
     end
 
     def new
@@ -23,11 +23,9 @@ class BlogPostsController < ApplicationController
     end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post
         else
@@ -36,7 +34,6 @@ class BlogPostsController < ApplicationController
     end
 
     def destroy 
-        @blog_post = BlogPost.find(params[:id])
         @blog_post.destroy
         redirect_to root_path
     end
@@ -44,6 +41,12 @@ class BlogPostsController < ApplicationController
     private
 
     def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body, :published_at)
+    end
+
+    def set_blog_post
+        @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
     end
 end
