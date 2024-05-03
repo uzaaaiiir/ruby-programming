9d7183e1f34a9ee971baa157935c6027ccb4298c
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2024-05-03 19:31:06 -0400

    Add tests for scheduling blog posts

--- code/gorails/blog/test/models/blog_post_test.rb
+++ code/gorails/blog/test/models/blog_post_test.rb
@@ -4,4 +4,44 @@ class BlogPostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "draft? returns true for draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for published blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "published? retrns true for published blog post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false for draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? retrns true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? returns false for published blog post" do
+    refute blog_posts(:published).scheduled?
+  end
+
+  def draft_blog_post
+    BlogPost.new(published_at: nil)
+  end
 end
