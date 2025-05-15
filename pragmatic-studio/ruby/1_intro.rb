bce1b9d7ab7a16b86c48fd29a377d29371f80069
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2025-05-14 23:02:17 -0400

    learn: intro to IRB, puts, and running ruby programs

---
+++ pragmatic-studio/ruby/1_intro.rb
@@ -0,0 +1,13 @@
+# IRB - interactive ruby
+# IRB follows a read-eval-print loop (REPL)
+# For example, 2 * 3 will be read, evaluated, and the 
+# result will be printed. Then it will loop again.
+
+# To run a ruby program: use command:
+# ruby <filename>.rb
+
+# puts - prints to the console
+puts"ruby is friendly!"
+
+phrase = "Ruby is fun!"
+puts phrase
